f7cbc1f161bad81ade4d67921dcb37f24bf5f1ac
Ainkaran Pathmanathan <pat.ainkaran@gmail.com>
2017-07-19 20:15:40 -0700

    add session, user, and blog models, and 3 controllers, setting up the show and index pages/my version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  get 'user/new'
 
-  # resources :blogs
+  get 'sessions/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :posts
+
+  resources :blogs
 
 end
